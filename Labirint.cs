b7dd1799ee7cfd7105543fd88499353feca86232
Makuta <64552136+Snakemonster@users.noreply.github.com>
2020-11-01 21:05:52 +0200

    Add files via upload

---
+++ Labirint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labirint
+{
+    public class LabirintCell
+    {
+        public int sizeX, sizeY;
+        public bool WallLeft, WallBottom;
+    }
+    class Labirint
+    {
+        public int Width = 20, Height = 30;
+        public LabirintCell[,] GenerateMaze()
+        {
+            LabirintCell[,] maze = new LabirintCell[Width, Height];
+            //creating labirint
+            //.     .       .
+
+            return maze;
+        }
+    }
+}
