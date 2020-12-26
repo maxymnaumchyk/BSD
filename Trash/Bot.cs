296ea0d70bcd8b7da58b7334d54283fe597e265e
Makuta <64552136+Snakemonster@users.noreply.github.com>
2020-12-26 15:35:09 +0200

    Add files via upload

---
+++ Trash/Bot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BOT
+{
+    public abstract class Bot
+    {
+        protected int x, y;
+        protected bool wallbottom = false, wallright = false, wallleft = false, wallup = false;
+        protected void Movement(string name, string vector)
+        {
+            Console.Write("Moving " + name + $"\t from:({x},{y})");
+            switch (vector)
+            {
+                case "right":
+                    if(!wallright) x++;
+                    break;
+                case "left":
+                    if(!wallleft) x--;
+                    break;
+                case "up":
+                    if(!wallup) y++;
+                    break;
+                case "down":
+                    if(!wallbottom) y--;
+                    break;
+            }
+            Console.WriteLine($"  to:({x},{y})");
+        }
+
+    }
+}
