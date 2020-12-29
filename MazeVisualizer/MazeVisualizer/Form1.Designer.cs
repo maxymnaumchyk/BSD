cd9c41070995a393f546f9bdda32e55877f76f28
maxymnaumchyk <70752300+maxymnaumchyk@users.noreply.github.com>
2020-12-29 12:55:59 +0200

    Add files via upload

--- MazeVisualizer/MazeVisualizer/Form1.Designer.cs
+++ MazeVisualizer/MazeVisualizer/Form1.Designer.cs
@@ -28,21 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pB
+            // 
+            this.pB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pB.Location = new System.Drawing.Point(12, 12);
+            this.pB.Name = "pB";
+            this.pB.Size = new System.Drawing.Size(500, 500);
+            this.pB.TabIndex = 0;
+            this.pB.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(574, 547);
+            this.Controls.Add(this.pB);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pB)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pB;
     }
 }
 
--- MazeVisualizer/MazeVisualizer/Form1.cs
+++ MazeVisualizer/MazeVisualizer/Form1.cs
@@ -15,20 +15,13 @@ namespace MazeVisualizer
 {
     public partial class Form1 : Form
     {
-        private Grid _grid;
         private BinaryTree _algorithm;
         public Form1()
         {
             InitializeComponent();
-            _grid = new Grid(10, 10);
-            pbMaze.Image = _grid.ToImg();
-            cbAlgorithm.SelectedIndex = 0;
-            _algorithm = new BinaryTree(_grid);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            pbMaze;
+            Grid _grid = new Grid(10, 10);
+            _grid = BinaryTree.Maze(_grid);
+            pB.Image = _grid.ToImg();
         }
     }
 }
