5c2d53fa5b85102788c700d97dae0e0d7e6b5f3c
Makuta <64552136+Snakemonster@users.noreply.github.com>
2020-12-30 23:20:09 +0200

    Add files via upload

---
+++ MazeTest/MazeFin/Maze/MazeVisualizer/Core/DistancesGrid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MazeGrid;
+using MazeDistances;
+using MazeCell;
+using MazeUtilities;
+namespace MazeDistancesGrid
+{
+
+    namespace MazeDistancesGrid
+    {
+        public class DistanceGrid : Grid
+        {
+            public Distances Distances { get; set; }
+
+            public DistanceGrid(int rows, int cols) : base(rows, cols) { }
+
+            protected override string ContentsOf(Cell cell)
+            {
+                if (Distances != null && Distances[cell] >= 0)
+                {
+                    return Distances[cell].ToBase64();
+                }
+                return base.ContentsOf(cell);
+            }
+        }
+    }
+}
---
+++ MazeTest/MazeFin/Maze/MazeVisualizer/Core/Utilities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeUtilities
+{
+    public static class IntExtensions
+    {
+        public static string ToBase36(this int i)
+        {
+            const string chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+            return chars[i % 36].ToString();
+        }
+
+        public static string ToBase64(this int i)
+        {
+            const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%=";
+            return chars[i % 64].ToString();
+        }
+    }
+
+    //Randomly select an element from a List<T> 
+    public static class ListExtensions
+    {
+        public static T Sample<T>(this List<T> list, Random rand = null)
+        {
+            if (rand == null)
+            {
+                rand = new Random();
+            }
+            return list[rand.Next(list.Count)];
+        }
+    }
+}
